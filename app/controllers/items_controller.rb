7c1cf1794e8a16ea38851c0a1de86d8853833d87
bondinu <spymissionwakuwaku06@gmail.com>
2023-07-06 21:54:09 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
   def Item_params
-    params.require(:item).permit(:title,:image).merge(user_id: current_user.id)#全部書く必要あり
+    params.require(:item).permit(:title,:description,:category_id,:status_id,:postage_id,:prefecture_id,:deadline_id,:cost,:image).merge(user_id: current_user.id)
   end
 end
