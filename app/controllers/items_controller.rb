eefb273577aae604d52dece761b1ff546e76367d
bondinu <spymissionwakuwaku06@gmail.com>
2023-07-06 20:59:18 +0900

    ActiveStorageを導入する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def Item_params
+    params.require(:item).permit(:title,:image).merge(user_id: current_user.id)#全部書く必要あり
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to:user
+  # has_one:order
+  has_one_attached :image
+
+  # validates :title          , presence: true
+  # validates :description    , presence: true
+  # validates :category_id    , presence: true
+  # validates :status_id      , presence: true
+  # validates :postage_id     , presence: true
+  # validates :prefecture_id  , presence: true
+  # validates :deadline_id    , presence: true
+  # validates :cost           , presence: true
 end
