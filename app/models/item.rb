0b04d985fc9325ac0572359ebf8c31a49e8e4ce1
bondinu <spymissionwakuwaku06@gmail.com>
2023-07-06 21:31:24 +0900

    ActiveHashの導入、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,22 @@ class Item < ApplicationRecord
   # has_one:order
   has_one_attached :image
 
-  # validates :title          , presence: true
-  # validates :description    , presence: true
-  # validates :category_id    , presence: true
-  # validates :status_id      , presence: true
-  # validates :postage_id     , presence: true
-  # validates :prefecture_id  , presence: true
-  # validates :deadline_id    , presence: true
-  # validates :cost           , presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :deadline
+
+  validates :title          , presence: true
+  validates :description    , presence: true
+  validates :cost           , presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id    , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id      , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :postage_id     , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id  , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :deadline_id    , numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
