27e449a65553aaacada65989e85ecbd1a0fbadbc
bondinu <spymissionwakuwaku06@gmail.com>
2023-07-29 16:21:41 +0900

    手順5 エラーの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,23 +5,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.create(order_params)
-    Address.create(address_params)
-    
-    redirect_to root_path
+    @item = Item.find(params[:item_id]) 
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def order_params
-   
-    params.permit(:item_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
   end
-
-  def address_params
-    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(order_id: @order.id)
-  end
-
-
 
 end
