9855f023890387399a7e9b9aaa99ee272a7ff5eb
bondinu <spymissionwakuwaku06@gmail.com>
2023-07-29 15:57:45 +0900

    手順4 フォームの情報の保存、購入の条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
     
+    redirect_to root_path
   end
+
+  private
+
+  def order_params
+   
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(order_id: @order.id)
+  end
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_07_24_114826) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "buliding"
+    t.string "building"
     t.string "phone_number", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
