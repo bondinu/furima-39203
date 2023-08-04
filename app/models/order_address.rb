c7b48ab892293b6c3cf5ffea60dc3d5c767487b7
bondinu <spymissionwakuwaku06@gmail.com>
2023-08-04 16:09:46 +0900

    修正３

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,8 +5,8 @@ class OrderAddress
   with_options presence: true do
     validates :token
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :address, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー0-9\- ]+\z/ }
+    validates :city
+    validates :address
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
     validates :item_id
     validates :user_id
